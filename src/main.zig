a0eee210c346136eb6940ae40cc7a040b212cfbd
el10savio <el10savio96@gmail.com>
2021-09-27 13:39:26 +0530

    Process Input Arguments

--- src/main.zig
+++ src/main.zig
@@ -1,26 +1,29 @@
 const std = @import("std");
+const fs = std.fs;
 
 pub fn main() anyerror!void {
     const stdout = std.io.getStdOut().writer();
 
     var allocator = std.heap.page_allocator;
-
     var iter = std.process.args();
+    _ = iter.skip();
+
     while (iter.next(allocator)) |arg| {
-        const argument = arg catch break;
-        try stdout.print("arg: {}\n", .{argument});
-        allocator.free(argument);
+        const path = arg catch break;
+        defer allocator.free(path);
+
+        try stdout.print("path: {}\n", .{path});
+        var file = try fs.cwd().openFile(path, .{});
+        defer file.close();
+
+        const stat = try file.stat();
+        try stdout.print("inode: {}\n", .{stat.inode});
+        try stdout.print("size: {}\n", .{stat.size});
+        try stdout.print("mode: {}\n", .{stat.mode});
+        try stdout.print("kind: {}\n", .{stat.kind});
+        try stdout.print("atime: {}\n", .{stat.atime});
+        try stdout.print("mtime: {}\n", .{stat.mtime});
+        try stdout.print("ctime: {}\n", .{stat.ctime});
+        try stdout.print("\n", .{});
     }
-
-    const file = try std.fs.cwd().createFile("file.txt", .{});
-    defer file.close();
-
-    const stat = try file.stat();
-    try stdout.print("inode: {}\n", .{stat.inode});
-    try stdout.print("size: {}\n", .{stat.size});
-    try stdout.print("mode: {}\n", .{stat.mode});
-    try stdout.print("kind: {}\n", .{stat.kind});
-    try stdout.print("atime: {}\n", .{stat.atime});
-    try stdout.print("mtime: {}\n", .{stat.mtime});
-    try stdout.print("ctime: {}\n", .{stat.ctime});
 }
