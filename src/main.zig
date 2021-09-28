9075bd58ce83ee3ee60d727377277850def8441e
el10savio <el10savio96@gmail.com>
2021-09-28 13:49:22 +0530

    Added Comments

--- src/main.zig
+++ src/main.zig
@@ -4,18 +4,25 @@ const fs = std.fs;
 pub fn main() anyerror!void {
     const stdout = std.io.getStdOut().writer();
 
+    // Process input arguments
     var allocator = std.heap.page_allocator;
     var iter = std.process.args();
+
+    // Skip binary name 
+    // which is the first argument
     _ = iter.skip();
 
+    // Iterate through all paths
     while (iter.next(allocator)) |arg| {
         const path = arg catch break;
         defer allocator.free(path);
 
+        // todo: Refactor to seperate function
         try stdout.print("path: {}\n", .{path});
         var file = try fs.cwd().openFile(path, .{});
         defer file.close();
 
+        // Get fs stat details and print
         const stat = try file.stat();
         try stdout.print("inode: {}\n", .{stat.inode});
         try stdout.print("size: {}\n", .{stat.size});
